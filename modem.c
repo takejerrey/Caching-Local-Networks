2d7c297aeac061116aacf0f2086804519b73a73d
Jacob <113203619+takejerrey@users.noreply.github.com>
2023-11-14 12:32:02 -0800

    Update modem.c

--- modem.c
+++ modem.c
@@ -9,7 +9,7 @@
 typedef enum {kData, kWifi, kUnknown} MEDIUM;
 
 typedef struct network {
- char network_name[20]; // change network_name from a pointer to a string
+ char network_name[20]; 
  int signal_strength;
  MEDIUM connection_medium;
  bool password_saved;
@@ -31,12 +31,7 @@ MEDIUM ConvertIntToMedium(int int_medium) {
   }
 }
 
-/**
-  * TODO: This function is buggy and not complete
-  *
-  * We should first fix this function and then work on fixing ScanNetworksV2().
-  * The fixes found in this function will help determine how to fix V2.
-  */
+
 void ScanNetworks(FILE *fp, char *connection_type) {
   char temp_name[20], password_saved[10];
   int i = 0, medium;
@@ -67,13 +62,6 @@ void ScanNetworks(FILE *fp, char *connection_type) {
   }
 }
 
-/**
-  * This function early-exits from networks that we don't already have access
-  * to. This way we can still scan for 5 networks, but we won't display/attempt
-  * to make a decision vs networks we don't have the password for.
-  *
-  * TODO: This function is buggy and not complete
-  */
 void ScanNetworksV2(FILE *fp, char *connection_type) {
 
   char temp_name[20], password_saved[10];
@@ -106,31 +94,7 @@ void ScanNetworksV2(FILE *fp, char *connection_type) {
 }
 
 
-void ScanNetworksV3() {
-  // If you were to iterate and modify ScanNetworksV2 to be even better,
-  // what would you do? You DO NOT need to write any code, you can just
-  // explain what you would do or write psuedocode.
-  /*
-  To improve upon ScanNetworksV2, I would dynamically allocate an array of cached networks
-  because I noticed that since we are only caching networks that we have the password for, 
-  there is some is a lot of memory left unused in the statically defined cached_networks array. To do 
-  this, I would use malloc to dynamically allocate enough memory for the name, signal strength, 
-  medium, and password whenever another element is to be added to the cached_networks array.
-  */
-}
 
-/**
-  * This function selects what network we should connect to based on certain
-  * criteria.
-  *
-  * 1. We should only choose networks that we can connect to
-  * 2. We should only connect to networks based on what criteria we want
-  *    (i.e., Wi-Fi, Data, either).
-  * 3. We should pick the network with the highest signal strength
-  *
-  * criteria    String denoting "wifi", "data", or "greedy"
-  * return      String of best network_name
-  */
 char* DetermineNetwork(char* criteria) {
   int maxSignalStrength = 0;
   char* best_network_name = NULL;
